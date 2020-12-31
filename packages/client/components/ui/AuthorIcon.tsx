7687871b1407fbff264e1b31a47ebfe8fe4a53f8
ProSavage <naman0311@gmail.com>
2020-12-30 23:58:53 -0600

    temp fix to get base live

--- packages/client/components/ui/AuthorIcon.tsx
+++ packages/client/components/ui/AuthorIcon.tsx
@@ -1,7 +1,8 @@
 import styled from "styled-components"
+import { User } from "../../types/User"
 
-export default function AuthorIcon(props: {author: AuthenticatorAssertionResponse, size: string}) {
-  return <Image src={getAuthorIconURL(props.author._id)} alt="" height={props.size}
+export default function AuthorIcon(props: {author: User, size: string}) {
+  return <Image src={"getAuthorIconURL(props.author._id)"} alt="" height={props.size}
 }
 
 const Image = styled.img`
