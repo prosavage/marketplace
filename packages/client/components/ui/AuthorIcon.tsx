537d2b6c9ca73d92b4dddc72d3c8dc102bb3d8bc
ProSavage <naman0311@gmail.com>
2020-12-30 23:59:42 -0600

    close author

--- packages/client/components/ui/AuthorIcon.tsx
+++ packages/client/components/ui/AuthorIcon.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components"
 import { User } from "../../types/User"
 
 export default function AuthorIcon(props: {author: User, size: string}) {
-  return <Image src={"getAuthorIconURL(props.author._id)"} alt="" height={props.size}
+  return <Image src={"getAuthorIconURL(props.author._id)"} alt="" height={props.size}/>
 }
 
 const Image = styled.img`
