e795a466c3f082eaebf1f2954589573ccb8eba7a
ProSavage <naman0311@gmail.com>
2021-01-26 18:19:55 -0600

    change dark color

--- packages/client/styles/theme/DarkTheme.ts
+++ packages/client/styles/theme/DarkTheme.ts
@@ -4,7 +4,7 @@ const DarkTheme: ITheme = {
     backgroundPrimary: "#111",
     backgroundSecondary: "black",
     color: "white",
-    accentColor: "#bedcfa",
+    accentColor: "#a7c5eb",
     secondaryAccentColor: "#98acf8",
     buttonAccent: "#2FB38B",
     borderColor: "#333",
