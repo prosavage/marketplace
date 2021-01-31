cd6ab0dd9600d809c5f7f820dda478f27623e083
JasonWet <jason@colonialpress.net>
2021-01-30 19:06:47 -0500

    added slight shadow

--- packages/client/components/ui/Button.tsx
+++ packages/client/components/ui/Button.tsx
@@ -3,13 +3,14 @@ import PropsTheme from "../../styles/theme/PropsTheme";
 
 const Button = styled.button`
   background: ${(props: PropsTheme) => props.theme.backgroundSecondary};
-  color: ${(props: PropsTheme) => props.theme.color};
+  color: ${(props: PropsTheme) => props.theme.accentColor};
   font-weight: bold;
   padding: 10px;
   outline: none;
   border: 1px solid ${(props: PropsTheme) => props.theme.borderColor};
   border-radius: 4px;
   cursor: pointer;
+  box-shadow: 0px 2px 3px rgba(0, 0, 0, 0.28);
 
   transition: 250ms ease-in-out;
   &:hover {
