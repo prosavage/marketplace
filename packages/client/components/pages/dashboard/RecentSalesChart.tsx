bf79642ad45dd63ab3a948ac5b71a37888903b01
prosavage <naman0311@gmail.com>
2021-03-01 00:09:29 -0600

    Performance optimized chart endpoints, filter from all data instead of refreshing data on select change

--- packages/client/components/pages/dashboard/RecentSalesChart.tsx
+++ packages/client/components/pages/dashboard/RecentSalesChart.tsx
@@ -1,171 +1,213 @@
-import {Area, AreaChart, ResponsiveContainer, Tooltip, XAxis} from "recharts";
-import {useRecoilValue} from "recoil";
-import {themeState} from "../../../atoms/theme";
-import React, {useEffect, useState} from "react";
+import { Area, AreaChart, ResponsiveContainer, Tooltip, XAxis } from "recharts";
+import { useRecoilValue } from "recoil";
+import { themeState } from "../../../atoms/theme";
+import React, { useEffect, useState } from "react";
 import getAxios from "../../../util/AxiosInstance";
 import Input from "../../ui/Input";
 import styled from "styled-components";
-import {parseISOString, validateISODate} from "../../../util/Validation";
+import { parseISOString, validateISODate } from "../../../util/Validation";
 import useToast from "../../../util/hooks/useToast";
 import PropsTheme from "../../../styles/theme/PropsTheme";
+import { Resource } from "../../../types/Resource";
 
 interface ChartDataNode {
-    sales: number;
-    date: string;
+  sales: number;
+  date: string;
 }
 
 export default function RecentSalesChart() {
-    const theme = useRecoilValue(themeState);
+  const theme = useRecoilValue(themeState);
 
-    const [select, setSelect] = useState("All");
+  const [select, setSelect] = useState("All");
 
-    const [data, setData] = useState<ChartDataNode[]>(generateData());
+  const [chartData, setChartData] = useState<ChartDataNode[]>([]);
 
-    const [start, setStart] = useState(
-        new Date(
-            new Date().getTime() - 60 * 60 * 24 * 7 * 1000
-        ).toLocaleDateString()
-    );
+  const [data, setData] = useState([]);
 
-    const [end, setEnd] = useState(new Date().toLocaleDateString())
+  const [resourceFilterOptions, setResourceOptions] = useState<Resource[]>([]);
 
-    const aggregateData = (data): ChartDataNode[] => {
-        const rawData = data.payload.payments;
-        const timeSorted = {};
+  const [start, setStart] = useState(
+    new Date(
+      new Date().getTime() - 60 * 60 * 24 * 7 * 1000
+    ).toLocaleDateString()
+  );
 
-        const startDate = parseISOString(start)
-        const endDate = parseISOString(end)
+  const [end, setEnd] = useState(new Date().toLocaleDateString());
 
-        let timeLooper = startDate.getTime()
-        while (timeLooper < endDate.getTime()) {
-            timeSorted[new Date(timeLooper).toLocaleDateString()] = 0
-            timeLooper = timeLooper + 1000 * 60 * 60 * 24
-        }
+  useEffect(() => {
+    if (data.length === 0) return;
+    console.log('processing data.')
+    transformDataToChartData();
+  }, [data]);
 
-        for (const payment of rawData) {
-            payment.timestamp = new Date(payment.timestamp);
-            const dateKey = payment.timestamp.toLocaleDateString();
-            timeSorted[dateKey] = timeSorted[dateKey] + 1;
-        }
+  useEffect(() => {
+    if (data.length === 0) return;
+    console.log('processing select triggered data...')
+    transformDataToChartData();
+  }, [select]);
 
-        const nodes = [];
+  const transformDataToChartData = () => {
+    let rawData = data;
 
-        for (const date of Object.keys(timeSorted)) {
-            const sales = timeSorted[date];
-            const node: ChartDataNode = {sales, date};
-            nodes.push(node);
-        }
+    if (select !== "All") {
+        console.log("filtering...")
+        rawData = rawData.filter(payment => payment.resource === select)
+    }
 
-        return nodes
+    const timeSorted = {};
+
+    const startDate = parseISOString(start);
+    const endDate = parseISOString(end);
+
+    let timeLooper = startDate.getTime();
+    while (timeLooper < endDate.getTime()) {
+      timeSorted[new Date(timeLooper).toLocaleDateString()] = 0;
+      timeLooper = timeLooper + 1000 * 60 * 60 * 24;
+    }
+
+    for (const payment of rawData) {
+      payment.timestamp = new Date(payment.timestamp);
+      const dateKey = payment.timestamp.toLocaleDateString();
+      timeSorted[dateKey] = timeSorted[dateKey] + 1;
+    }
+
+    const nodes = [];
+
+    for (const date of Object.keys(timeSorted)) {
+      const sales = timeSorted[date];
+      const node: ChartDataNode = { sales, date };
+      nodes.push(node);
+    }
+    setChartData(nodes);
+  };
+
+  const toast = useToast();
+
+  useEffect(() => {
+    if (!validateISODate(start) || !validateISODate(end)) {
+      return;
+    }
+
+    const startDate = parseISOString(start);
+    const endDate = parseISOString(end);
+
+    const filter: any = {
+      start: startDate.getTime(),
+      end: endDate.getTime(),
     };
+    if (select !== "All") {
+      filter.resource = select;
+    }
+    getAxios()
+      .post("/checkout/purchase-chart", filter)
+      .then((res) => {
+        setData(res.data.payload.payments);
+      });
+  }, [start, end]);
 
-    const toast = useToast()
+  useEffect(() => {
+    getAxios()
+      .get("/directory/my-resources")
+      .then((res) => {
+        const paidResources = res.data.payload.resources.filter(
+          (resource: Resource) => resource.price !== 0
+        );
+        setResourceOptions(paidResources);
+      })
+      .catch((err) => toast(err.response.data));
+  }, []);
 
-    useEffect(() => {
-        if (!validateISODate(start) || !validateISODate(end)) {
-            return
-        }
-
-        const startDate = parseISOString(start)
-        const endDate = parseISOString(end)
-
-
-        const filter: any = {
-            start: startDate.getTime(),
-            end: endDate.getTime(),
-        };
-        if (select !== "All") {
-            filter.resource = select;
-        }
-        getAxios()
-            .post("/checkout/purchase-chart", filter)
-            .then((res) => setData(aggregateData(res.data)));
-    }, [select, start, end]);
-
-    return (
-        <ChartContainer>
-            <h1>Daily Sales</h1>
-            <Controls>
-                <ControlContainer>
-                    <Select
-                        name={"resource"}
-                        value={select}
-                        onChange={(e) => setSelect(e.target.value)}
-                    >
-                        <option value={"All"}>All Resources</option>
-                        <option value={"CVE0kzSpW"}>FactionsX</option>
-                        <option value={"eUyVEKcjm"}>SkyblockX</option>
-                        <option value={"CSMVL75iD"}>TestResource</option>
-                    </Select>
-                </ControlContainer>
-                <ControlGroup>
-                    <ControlContainer>
-                        <label>Start Date</label>
-                        <Input value={start}
-                               onChange={(e) => setStart(e.target.value)}
-                               invalid={!validateISODate(start)
-                               }/>
-                    </ControlContainer>
-                    <ControlContainer>
-                        <label>End Date</label>
-                        <Input value={end} onChange={(e) => setEnd(e.target.value)} invalid={false}/>
-                    </ControlContainer>
-                </ControlGroup>
-            </Controls>
-            <PaddedResponsiveContainer width={"100%"} height={250}>
-                <AreaChart
-                    data={data}
-                    margin={{top: 0, right: 0, left: 0, bottom: 0}}
-                >
-                    <defs>
-                        <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-                            <stop
-                                offset="10%"
-                                stopColor={theme.accentColor}
-                                stopOpacity={0.8}
-                            />
-                            <stop
-                                offset="90%"
-                                stopColor={theme.accentColor}
-                                stopOpacity={0.3}
-                            />
-                        </linearGradient>
-                    </defs>
-                    <XAxis dataKey={"date"}/>
-                    {/* Auto sends the props :) */}
-                    <Tooltip content={<CustomTooltip/>}/>
-                    <Area
-                        type="monotone"
-                        dataKey="sales"
-                        stroke={theme.accentColor}
-                        fillOpacity={1}
-                        fill="url(#colorPv)"
-                    />
-                </AreaChart>
-            </PaddedResponsiveContainer>
-        </ChartContainer>
-    );
+  return (
+    <ChartContainer>
+      <h1>Daily Sales</h1>
+      <Controls>
+        <ControlContainer>
+          <Select
+            name={"resource"}
+            value={select}
+            onChange={(e) => setSelect(e.target.value)}
+          >
+            <option value={"All"}>All Resources</option>
+            {resourceFilterOptions.map((opt) => (
+              <option key={opt._id} value={opt._id}>
+                {opt.name}
+              </option>
+            ))}
+          </Select>
+        </ControlContainer>
+        <ControlGroup>
+          <ControlContainer>
+            <label>Start Date</label>
+            <Input
+              value={start}
+              onChange={(e) => setStart(e.target.value)}
+              invalid={!validateISODate(start)}
+            />
+          </ControlContainer>
+          <ControlContainer>
+            <label>End Date</label>
+            <Input
+              value={end}
+              onChange={(e) => setEnd(e.target.value)}
+              invalid={false}
+            />
+          </ControlContainer>
+        </ControlGroup>
+      </Controls>
+      <PaddedResponsiveContainer width={"100%"} height={250}>
+        <AreaChart
+          data={chartData}
+          margin={{ top: 0, right: 0, left: 0, bottom: 0 }}
+        >
+          <defs>
+            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+              <stop
+                offset="10%"
+                stopColor={theme.accentColor}
+                stopOpacity={0.8}
+              />
+              <stop
+                offset="90%"
+                stopColor={theme.accentColor}
+                stopOpacity={0.3}
+              />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey={"date"} />
+          {/* Auto sends the props :) */}
+          <Tooltip content={<CustomTooltip />} />
+          <Area
+            type="monotone"
+            dataKey="sales"
+            stroke={theme.accentColor}
+            fillOpacity={1}
+            fill="url(#colorPv)"
+          />
+        </AreaChart>
+      </PaddedResponsiveContainer>
+    </ChartContainer>
+  );
 }
 
 const CustomTooltip = ({
-                           active,
-                           payload,
-                           label,
-                       }: {
-    active?: any;
-    payload?: any;
-    label?: any;
+  active,
+  payload,
+  label,
+}: {
+  active?: any;
+  payload?: any;
+  label?: any;
 }) => {
-    if (active) {
-        return (
-            <div>
-                <p>{`${payload ? Math.round(payload[0]?.value) : 0} sales`}</p>
-                <p>{label}</p>
-            </div>
-        );
-    }
+  if (active) {
+    return (
+      <div>
+        <p>{`${payload ? Math.round(payload[0]?.value) : 0} sales`}</p>
+        <p>{label}</p>
+      </div>
+    );
+  }
 
-    return null;
+  return null;
 };
 
 const ChartContainer = styled.div`
@@ -180,8 +222,12 @@ const Select = styled.select`
   border-radius: 5px;
   border-color: ${(props: PropsTheme) => props.theme.accentColor};
   background: transparent;
-  color: ${(props: PropsTheme) => props.theme.color}
-`
+  color: ${(props: PropsTheme) => props.theme.color};
+
+  option {
+    color: black;
+  }
+`;
 
 const PaddedResponsiveContainer = styled(ResponsiveContainer)``;
 
@@ -198,60 +244,18 @@ const ControlContainer = styled.div`
   flex-direction: column;
   margin-bottom: 1em;
   padding: 0 0.5em;
-`
+`;
 
 const ControlGroup = styled.div`
   display: flex;
-`
-
+`;
 
 function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max)) + 20;
+  return Math.floor(Math.random() * Math.floor(max)) + 20;
 }
 
 function date(subtract) {
-    return new Date(
-        new Date().getTime() - subtract * 60 * 60 * 24 * 1000
-    ).toLocaleDateString();
+  return new Date(
+    new Date().getTime() - subtract * 60 * 60 * 24 * 1000
+  ).toLocaleDateString();
 }
-
-const generateData = () => {
-    return [
-        {
-            sales: getRandomInt(100),
-            date: date(1),
-        },
-        {
-            sales: getRandomInt(100),
-            date: date(2),
-        },
-        {
-            sales: getRandomInt(100),
-            date: date(3),
-        },
-        {
-            sales: getRandomInt(100),
-            date: date(4),
-        },
-        {
-            sales: getRandomInt(100),
-            date: date(5),
-        },
-        {
-            sales: getRandomInt(100),
-            date: date(6),
-        },
-        {
-            sales: getRandomInt(100),
-            date: date(7),
-        },
-        {
-            sales: getRandomInt(100),
-            date: date(8),
-        },
-        {
-            sales: getRandomInt(100),
-            date: date(9),
-        },
-    ].reverse();
-};
