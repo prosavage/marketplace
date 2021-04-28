1e3ded42fab954311810e43e6ec744b96ad41884
ProSavage <naman0311@gmail.com>
2021-04-28 03:25:37 -0500

    Dont show resource ratings if owner

--- packages/client/components/pages/resource/ReviewEntry.tsx
+++ packages/client/components/pages/resource/ReviewEntry.tsx
@@ -1,73 +1,79 @@
-import {useEffect, useState} from "react";
-import {useRecoilValue} from "recoil";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { MessageSquare, Trash } from "react-feather";
+import { useRecoilValue } from "recoil";
 import styled from "styled-components";
-import {themeState} from "../../../atoms/theme";
+import timeago from "time-ago";
+import { themeState } from "../../../atoms/theme";
+import { userState } from "../../../atoms/user";
 import PropsTheme from "../../../styles/theme/PropsTheme";
-import {Review} from "../../../types/Review";
+import { Review } from "../../../types/Review";
+import { User } from "../../../types/User";
 import getAxios from "../../../util/AxiosInstance";
+import useToast from "../../../util/hooks/useToast";
 import renderReviewDroplets from "../../../util/Review";
 import AuthorIcon from "../../ui/AuthorIcon";
-import timeago from "time-ago";
-import {User} from "../../../types/User";
-import Link from "next/link";
-import {MessageSquare, Trash} from "react-feather";
-import useToast from "../../../util/hooks/useToast";
-import {userState} from "../../../atoms/user";
 
-export default function ReviewEntry({review, onDelete}: { review: Review, onDelete: () => void }) {
-    const [user, setUser] = useState<User>();
-    const theme = useRecoilValue(themeState);
-    const toast = useToast();
+export default function ReviewEntry({
+  review,
+  onDelete,
+}: {
+  review?: Review;
+  onDelete: () => void;
+}) {
+  const [user, setUser] = useState<User>();
+  const theme = useRecoilValue(themeState);
+  const toast = useToast();
 
-    const me = useRecoilValue(userState);
+  const me = useRecoilValue(userState);
 
-    useEffect(() => {
-        getAxios()
-            .get(`/directory/user/${review.author}`)
-            .then((res) => {
-                setUser(res.data.payload.user);
-            });
-    }, []);
+  useEffect(() => {
+    getAxios()
+      .get(`/directory/user/${review.author}`)
+      .then((res) => {
+        setUser(res.data.payload.user);
+      });
+  }, []);
 
-    const deleteReview = () => {
-        getAxios()
-            .delete(`/review/${review._id}`)
-            .then((res) => {
-                toast("Review deleted successfully");
-                onDelete();
-            })
-            .catch((err) => console.log(err.response.data));
-    };
+  const deleteReview = () => {
+    getAxios()
+      .delete(`/review/${review._id}`)
+      .then((res) => {
+        toast("Review deleted successfully");
+        onDelete();
+      })
+      .catch((err) => console.log(err.response.data));
+  };
 
-    return (
-        <Wrapper>
-            <AuthorIcon user={user} size={"96px"}/>
-            <Content>
-                <BottomRow>
-                    <Link href={"/users/[id]"} as={`/users/${user?._id}`}>
-                        <AuthorLink>{user?.username}</AuthorLink>
-                    </Link>
-                    <ReviewDropsContainer>
-                        {renderReviewDroplets(theme, review.rating)}
-                    </ReviewDropsContainer>
-                </BottomRow>
-                <Message>{review.message}</Message>
-                <BottomRow>
-                    <p>{timeago.ago(review.timestamp)}</p>
-                    <ButtonContainer>
-                        <Spacer>
-                            <MessageSquare onClick={() => toast(`Coming Soon`)}/>
-                        </Spacer>
-                        {me?._id === user?._id && (
-                            <Spacer>
-                                <Trash onClick={deleteReview}/>
-                            </Spacer>
-                        )}
-                    </ButtonContainer>
-                </BottomRow>
-            </Content>
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <AuthorIcon user={user} size={"96px"} />
+      <Content>
+        <BottomRow>
+          <Link href={"/users/[id]"} as={`/users/${user?._id}`}>
+            <AuthorLink>{user?.username}</AuthorLink>
+          </Link>
+          <ReviewDropsContainer>
+            {renderReviewDroplets(theme, review.rating)}
+          </ReviewDropsContainer>
+        </BottomRow>
+        <Message>{review.message}</Message>
+        <BottomRow>
+          <p>{timeago.ago(review.timestamp)}</p>
+          <ButtonContainer>
+            <Spacer>
+              <MessageSquare onClick={() => toast(`Coming Soon`)} />
+            </Spacer>
+            {me?._id === user?._id && (
+              <Spacer>
+                <Trash onClick={deleteReview} />
+              </Spacer>
+            )}
+          </ButtonContainer>
+        </BottomRow>
+      </Content>
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.div`
@@ -123,4 +129,4 @@ const AuthorLink = styled.p`
 // Important or you can send one LONG line with no space between and ruin styling.
 const Message = styled.p`
   word-break: break-all;
-`
+`;
