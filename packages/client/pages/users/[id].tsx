f314328b130d5e806b537d6332bd0218d1ddbe89
ProSavage <naman0311@gmail.com>
2021-06-08 21:54:50 -0500

    Fix compiler issue with user

--- packages/client/pages/users/[id].tsx
+++ packages/client/pages/users/[id].tsx
@@ -27,11 +27,11 @@ export default function UserById(props: { id: string }) {
       <h1>Author Profile</h1>
       <UserHeader user={user} stats={stats} />
       <ResourcesContainer>
-        <ResourceList
+        {/* <ResourceList
           type={ResourceType.PLUGIN}
           category={undefined}
           team={user}
-        />
+        /> */}
       </ResourcesContainer>
     </Wrapper>
   );
