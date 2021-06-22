d8a32cab91fdb40fec7d0c7f8ed8be3de9d7a281
ProSavage <naman0311@gmail.com>
2021-06-21 19:06:42 -0500

    generate empty nodes when no sales on dashboard :D

--- packages/client/components/pages/dashboard/RecentSalesChart.tsx
+++ packages/client/components/pages/dashboard/RecentSalesChart.tsx
@@ -1,252 +1,271 @@
-import React, { useEffect, useState } from "react";
-import { Area, AreaChart, ResponsiveContainer, Tooltip, XAxis } from "recharts";
-import { useRecoilValue } from "recoil";
+import React, {useEffect, useState} from "react";
+import {Area, AreaChart, ResponsiveContainer, Tooltip, XAxis} from "recharts";
+import {useRecoilValue} from "recoil";
 import styled from "styled-components";
-import { themeState } from "../../../atoms/theme";
+import {themeState} from "../../../atoms/theme";
 import PropsTheme from "../../../styles/theme/PropsTheme";
-import { Resource } from "@savagelabs/types";
+import {Resource} from "@savagelabs/types";
 import getAxios from "../../../util/AxiosInstance";
-import { formatNumber } from "../../../util/Format";
+import {formatNumber} from "../../../util/Format";
 import useToast from "../../../util/hooks/useToast";
-import { parseISOString, validateISODate } from "../../../util/Validation";
+import {parseISOString, validateISODate} from "../../../util/Validation";
 import Input from "../../ui/Input";
 
 interface ChartDataNode {
-  sales: number;
-  amount: number;
-  date: string;
+    sales: number;
+    amount: number;
+    date: string;
 }
 
 export default function RecentSalesChart() {
-  const theme = useRecoilValue(themeState);
+    const theme = useRecoilValue(themeState);
 
-  const [select, setSelect] = useState("All");
+    const [select, setSelect] = useState("All");
 
-  const [chartData, setChartData] = useState<ChartDataNode[]>([]);
+    const [chartData, setChartData] = useState<ChartDataNode[]>([]);
 
-  const [data, setData] = useState([]);
+    const [data, setData] = useState([]);
 
-  const [resourceFilterOptions, setResourceOptions] = useState<Resource[]>([]);
+    const [resourceFilterOptions, setResourceOptions] = useState<Resource[]>([]);
 
-  const [start, setStart] = useState(
-    new Date(
-      new Date().getTime() - 60 * 60 * 24 * 7 * 1000
-    ).toLocaleDateString()
-  );
+    const aboutAWeekAgo = new Date(new Date().getTime() - 60 * 60 * 24 * 7 * 1000)
 
-  const [end, setEnd] = useState(new Date().toLocaleDateString());
+    const [start, setStart] = useState(
+        aboutAWeekAgo.toLocaleDateString()
+    );
 
-  useEffect(() => {
-    if (data.length === 0) return;
-    transformDataToChartData();
-  }, [data]);
+    const [end, setEnd] = useState(new Date().toLocaleDateString());
 
-  useEffect(() => {
-    if (data.length === 0) return;
-    transformDataToChartData();
-  }, [select]);
-
-  const transformDataToChartData = () => {
-    let rawData = data;
-
-    if (select !== "All") {
-      rawData = rawData.filter((payment) => payment.resource === select);
+    // for when we have no data we can show empty chart.
+    const generateDummyData = () => {
+        const dummyData: ChartDataNode[] = [];
+        const now = new Date()
+        let starter = new Date(aboutAWeekAgo);
+        const ONE_DAY = 60 * 60 * 24 * 1000
+        while (starter.getTime() <= now.getTime()) {
+            dummyData.push({sales: 0, amount: 0, date: starter.toLocaleDateString()})
+            starter = new Date(starter.getTime() + ONE_DAY)
+        }
+        setChartData(dummyData);
     }
 
-    const timeSorted = {};
 
-    const startDate = parseISOString(start);
-    const endDate = parseISOString(end);
+    useEffect(() => {
+        if (data.length === 0) return;
+        transformDataToChartData();
+    }, [data]);
 
-    let timeLooper = startDate.getTime();
-    const oneDay = 1000 * 60 * 60 * 24;
-    while (timeLooper < endDate.getTime()) {
-      timeSorted[new Date(timeLooper).toLocaleDateString()] = {
-        sales: 0,
-        amount: 0,
-      };
-      timeLooper = timeLooper + oneDay;
-    }
+    useEffect(() => {
+        if (data.length === 0) return;
+        transformDataToChartData();
+    }, [select]);
 
-    for (const payment of rawData) {
-      payment.timestamp = new Date(payment.timestamp);
-      const dateKey = payment.timestamp.toLocaleDateString();
-      timeSorted[dateKey] = {
-        sales: timeSorted[dateKey].sales + 1,
-        amount: timeSorted[dateKey].amount + payment.amount,
-      };
-    }
+    const transformDataToChartData = () => {
+        let rawData = data;
 
-    const nodes = [];
+        if (select !== "All") {
+            rawData = rawData.filter((payment) => payment.resource === select);
+        }
 
-    for (const date of Object.keys(timeSorted)) {
-      const sales = timeSorted[date].sales;
-      const amount = timeSorted[date].amount;
-      const node: ChartDataNode = { sales, amount, date };
-      nodes.push(node);
-    }
-    setChartData(nodes);
-  };
+        const timeSorted = {};
 
-  const toast = useToast();
+        const startDate = parseISOString(start);
+        const endDate = parseISOString(end);
 
-  useEffect(() => {
-    if (!validateISODate(start) || !validateISODate(end)) {
-      return;
-    }
+        let timeLooper = startDate.getTime();
+        const oneDay = 1000 * 60 * 60 * 24;
+        while (timeLooper < endDate.getTime()) {
+            timeSorted[new Date(timeLooper).toLocaleDateString()] = {
+                sales: 0,
+                amount: 0,
+            };
+            timeLooper = timeLooper + oneDay;
+        }
 
-    const startDate = parseISOString(start);
-    const endDate = parseISOString(end);
+        for (const payment of rawData) {
+            payment.timestamp = new Date(payment.timestamp);
+            const dateKey = payment.timestamp.toLocaleDateString();
+            timeSorted[dateKey] = {
+                sales: timeSorted[dateKey].sales + 1,
+                amount: timeSorted[dateKey].amount + payment.amount,
+            };
+        }
 
-    const filter: any = {
-      start: startDate.getTime(),
-      end: endDate.getTime(),
+        const nodes = [];
+
+        for (const date of Object.keys(timeSorted)) {
+            const sales = timeSorted[date].sales;
+            const amount = timeSorted[date].amount;
+            const node: ChartDataNode = {sales, amount, date};
+            nodes.push(node);
+        }
+        setChartData(nodes);
     };
-    if (select !== "All") {
-      filter.resource = select;
-    }
-    getAxios()
-      .post("/checkout/purchase-chart", filter)
-      .then((res) => {
-        setData(res.data.payload.payments);
-      });
-  }, [start, end]);
 
-  useEffect(() => {
-    getAxios()
-      .get("/directory/my-resources")
-      .then((res) => {
-        const paidResources = res.data.payload.resources.filter(
-          (resource: Resource) => resource.price !== 0
-        );
-        setResourceOptions(paidResources);
-      })
-      .catch((err) => toast(err.response.data.error));
-  }, []);
+    const toast = useToast();
 
-  return (
-    <ChartContainer>
-      <h1>Daily Sales</h1>
-      <Controls>
-        <ControlContainer>
-          <Select
-            name={"resource"}
-            value={select}
-            onChange={(e) => setSelect(e.target.value)}
-          >
-            <option value={"All"}>All Resources</option>
-            {resourceFilterOptions.map((opt) => (
-              <option key={opt._id} value={opt._id}>
-                {opt.name}
-              </option>
-            ))}
-          </Select>
-        </ControlContainer>
-        <ControlGroup>
-          <ControlContainer>
-            <label>Start Date</label>
-            <Input
-              value={start}
-              onChange={(e) => setStart(e.target.value)}
-              invalid={!validateISODate(start)}
-            />
-          </ControlContainer>
-          <ControlContainer>
-            <label>End Date</label>
-            <Input
-              value={end}
-              onChange={(e) => setEnd(e.target.value)}
-              invalid={false}
-            />
-          </ControlContainer>
-        </ControlGroup>
-      </Controls>
+    useEffect(() => {
+        if (!validateISODate(start) || !validateISODate(end)) {
+            return;
+        }
 
-      {chartData.length > 0 && (
-        <ResponsiveContainer width={"100%"} height={250}>
-          <AreaChart
-            data={chartData}
-            margin={{ top: 0, right: 0, left: 0, bottom: 0 }}
-          >
-            <defs>
-              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-                <stop
-                  offset="10%"
-                  stopColor={theme.accentColor}
-                  stopOpacity={0.8}
-                />
-                <stop
-                  offset="90%"
-                  stopColor={theme.accentColor}
-                  stopOpacity={0.3}
-                />
-              </linearGradient>
-            </defs>
-            <XAxis dataKey={"date"} />
-            {/* Auto sends the props :) */}
-            <Tooltip content={<CustomTooltip />} />
-            <Area
-              type="monotone"
-              dataKey="sales"
-              stroke={theme.accentColor}
-              fillOpacity={1}
-              fill="url(#colorPv)"
-            />
-          </AreaChart>
-        </ResponsiveContainer>
-      )}
-      <Controls>
-        <ControlGroup>
-          <ControlContainer>
-            <label>Total Profit</label>
-            <h2>
-              $
-              {chartData.length > 0
-                ? formatNumber(
-                    chartData
-                      .map((node) => node.amount)
-                      .reduce((v1, v2) => v1 + v2) / 100
-                  )
-                : 0}
-            </h2>
-          </ControlContainer>
-          <ControlContainer>
-            <label>Total Sales</label>
-            <h2>
-              {chartData.length > 0
-                ? formatNumber(
-                    chartData
-                      .map((node) => node.sales)
-                      .reduce((v1, v2) => v1 + v2)
-                  )
-                : 0}
-            </h2>
-          </ControlContainer>
-        </ControlGroup>
-      </Controls>
-    </ChartContainer>
-  );
+        const startDate = parseISOString(start);
+        const endDate = parseISOString(end);
+
+        const filter: any = {
+            start: startDate.getTime(),
+            end: endDate.getTime(),
+        };
+        if (select !== "All") {
+            filter.resource = select;
+        }
+        getAxios()
+            .post("/checkout/purchase-chart", filter)
+            .then((res) => {
+                const payments = res.data.payload.payments;
+                if (payments.length !== 0) {
+                    setData(res.data.payload.payments);
+                } else {
+                  generateDummyData();
+                }
+            });
+    }, [start, end]);
+
+    useEffect(() => {
+        getAxios()
+            .get("/directory/my-resources")
+            .then((res) => {
+                const paidResources = res.data.payload.resources.filter(
+                    (resource: Resource) => resource.price !== 0
+                );
+                setResourceOptions(paidResources);
+            })
+            .catch((err) => toast(err.response.data.error));
+    }, []);
+
+    return (
+        <ChartContainer>
+            <h1>Daily Sales</h1>
+            <Controls>
+                <ControlContainer>
+                    <Select
+                        name={"resource"}
+                        value={select}
+                        onChange={(e) => setSelect(e.target.value)}
+                    >
+                        <option value={"All"}>All Resources</option>
+                        {resourceFilterOptions.map((opt) => (
+                            <option key={opt._id} value={opt._id}>
+                                {opt.name}
+                            </option>
+                        ))}
+                    </Select>
+                </ControlContainer>
+                <ControlGroup>
+                    <ControlContainer>
+                        <label>Start Date</label>
+                        <Input
+                            value={start}
+                            onChange={(e) => setStart(e.target.value)}
+                            invalid={!validateISODate(start)}
+                        />
+                    </ControlContainer>
+                    <ControlContainer>
+                        <label>End Date</label>
+                        <Input
+                            value={end}
+                            onChange={(e) => setEnd(e.target.value)}
+                            invalid={false}
+                        />
+                    </ControlContainer>
+                </ControlGroup>
+            </Controls>
+
+            {(
+                <ResponsiveContainer width={"100%"} height={250}>
+                    <AreaChart
+                        data={chartData}
+                        margin={{top: 0, right: 0, left: 0, bottom: 0}}
+                    >
+                        <defs>
+                            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                                <stop
+                                    offset="10%"
+                                    stopColor={theme.accentColor}
+                                    stopOpacity={0.8}
+                                />
+                                <stop
+                                    offset="90%"
+                                    stopColor={theme.accentColor}
+                                    stopOpacity={0.3}
+                                />
+                            </linearGradient>
+                        </defs>
+                        <XAxis dataKey={"date"}/>
+                        {/* Auto sends the props :) */}
+                        <Tooltip content={<CustomTooltip/>}/>
+                        <Area
+                            type="monotone"
+                            dataKey="sales"
+                            stroke={theme.accentColor}
+                            fillOpacity={1}
+                            fill="url(#colorPv)"
+                        />
+                    </AreaChart>
+                </ResponsiveContainer>
+            )}
+            <Controls>
+                <ControlGroup>
+                    <ControlContainer>
+                        <label>Total Profit</label>
+                        <h2>
+                            $
+                            {data.length > 0
+                                ? formatNumber(
+                                    chartData
+                                        .map((node) => node.amount)
+                                        .reduce((v1, v2) => v1 + v2) / 100
+                                )
+                                : 0}
+                        </h2>
+                    </ControlContainer>
+                    <ControlContainer>
+                        <label>Total Sales</label>
+                        <h2>
+                            {data.length > 0
+                                ? formatNumber(
+                                    chartData
+                                        .map((node) => node.sales)
+                                        .reduce((v1, v2) => v1 + v2)
+                                )
+                                : 0}
+                        </h2>
+                    </ControlContainer>
+                </ControlGroup>
+            </Controls>
+        </ChartContainer>
+    );
 }
 
 const CustomTooltip = ({
-  active,
-  payload,
-  label,
-}: {
-  active?: any;
-  payload?: any;
-  label?: any;
+                           active,
+                           payload,
+                           label,
+                       }: {
+    active?: any;
+    payload?: any;
+    label?: any;
 }) => {
-  if (active) {
-    return (
-      <div>
-        <p>{`${payload ? Math.round(payload[0]?.value) : 0} sales`}</p>
-        <p>{label}</p>
-      </div>
-    );
-  }
+    if (active) {
+        return (
+            <div>
+                <p>{`${payload ? Math.round(payload[0]?.value) : 0} sales`}</p>
+                <p>{label}</p>
+            </div>
+        );
+    }
 
-  return null;
+    return null;
 };
 
 const ChartContainer = styled.div`
--- packages/server/src/routes/checkout/CheckoutInfoRouter.ts
+++ packages/server/src/routes/checkout/CheckoutInfoRouter.ts
@@ -1,170 +1,166 @@
-import { Payment, PaymentStatus, Seller } from "@savagelabs/types";
+import {Payment, PaymentStatus, Seller} from "@savagelabs/types";
 import Axios from "axios";
-import express, { Request, Response } from "express";
-import { body, param } from "express-validator";
+import express, {Request, Response} from "express";
+import {body, param} from "express-validator";
 import shortid from "shortid";
-import {
-  PAYMENTS_COLLECTION,
-  RESOURCES_COLLECTION,
-  SELLER_COLLECTION,
-  USERS_COLLECTION,
-} from "../../constants";
-import { Authorize } from "../../middleware/Authenticate";
-import { isValidBody } from "../../middleware/BodyValidate";
-import { getDatabase } from "../../server";
+import {PAYMENTS_COLLECTION, RESOURCES_COLLECTION, SELLER_COLLECTION, USERS_COLLECTION,} from "../../constants";
+import {Authorize} from "../../middleware/Authenticate";
+import {isValidBody} from "../../middleware/BodyValidate";
+import {getDatabase} from "../../server";
 import getStripeAPI from "./stripe/StripeAPI";
 
 const checkoutInfoRouter = express.Router();
 
 checkoutInfoRouter.get(
-  "/link",
-  [Authorize, isValidBody],
-  async (req: Request, res: Response) => {
-    const seller = await getDatabase()
-      .collection<Seller>(SELLER_COLLECTION)
-      .findOne({ user: req.user!!._id });
+    "/link",
+    [Authorize, isValidBody],
+    async (req: Request, res: Response) => {
+        const seller = await getDatabase()
+            .collection<Seller>(SELLER_COLLECTION)
+            .findOne({user: req.user!!._id});
 
-    if (seller === null) {
-      res.failure("seller not found.");
-      return;
+        if (seller === null) {
+            res.failure("seller not found.");
+            return;
+        }
+        const stripeAPI = getStripeAPI();
+
+        const link = await stripeAPI.accounts.createLoginLink(
+            seller.stripe_account,
+            {apiKey: process.env.STRIPE_SECRET_KEY}
+        );
+
+        res.success({link: link});
     }
-    const stripeAPI = getStripeAPI();
-
-    const link = await stripeAPI.accounts.createLoginLink(
-      seller.stripe_account,
-      { apiKey: process.env.STRIPE_SECRET_KEY }
-    );
-
-    res.success({ link: link });
-  }
 );
 
 checkoutInfoRouter.get(
-  "/balance",
-  [Authorize, isValidBody],
-  async (req: Request, res: Response) => {
-    const seller = await getDatabase()
-      .collection<Seller>(SELLER_COLLECTION)
-      .findOne({ user: req.user!!._id });
+    "/balance",
+    [Authorize, isValidBody],
+    async (req: Request, res: Response) => {
+        const seller = await getDatabase()
+            .collection<Seller>(SELLER_COLLECTION)
+            .findOne({user: req.user!!._id});
 
-    if (seller === null) {
-      res.failure("seller not found.");
-      return;
+        if (seller === null) {
+            res.failure("seller not found.");
+            return;
+        }
+
+        const balance = await Axios.get("https://api.stripe.com/v1/balance", {
+            headers: {
+                Authorization: `Bearer ${process.env.STRIPE_SECRET_KEY}`,
+                "Stripe-Account": seller.stripe_account,
+            },
+        });
+
+        res.success({balance: balance.data});
     }
-
-    const balance = await Axios.get("https://api.stripe.com/v1/balance", {
-      headers: {
-        Authorization: `Bearer ${process.env.STRIPE_SECRET_KEY}`,
-        "Stripe-Account": seller.stripe_account,
-      },
-    });
-
-    res.success({ balance: balance.data });
-  }
 );
 
 checkoutInfoRouter.get(
-  "/purchases/:page",
-  [param("page").isNumeric(), Authorize, isValidBody],
-  async (req: Request, res: Response) => {
-    const page = req.params.page as unknown as number;
+    "/purchases/:page",
+    [param("page").isNumeric(), Authorize, isValidBody],
+    async (req: Request, res: Response) => {
+        const page = req.params.page as unknown as number;
 
-    const pageAmount = 5;
+        const pageAmount = 5;
 
-    const payments = await getDatabase()
-      .collection<Payment>(PAYMENTS_COLLECTION)
-      .aggregate([
-        {
-          $match: {
-            recipient: req.user!!._id,
-            status: PaymentStatus.CONFIRMED,
-          },
-        },
-        { $sort: { timestamp: -1 } },
-        { $skip: (page - 1) * pageAmount },
-        { $limit: pageAmount },
-        {
-          $lookup: {
-            from: RESOURCES_COLLECTION,
-            localField: "resource",
-            foreignField: "_id",
-            as: "resource",
-          },
-        },
-        {
-          $lookup: {
-            from: USERS_COLLECTION,
-            localField: "user",
-            foreignField: "_id",
-            as: "user",
-          },
-        },
-        { $unwind: "$user" },
-        { $unwind: "$resource" },
-        {
-          $unset: [
-            "user.email",
-            "user.role",
-            "user.password",
-            "user.purchases",
-          ],
-        },
-      ])
-      .toArray();
+        const payments = await getDatabase()
+            .collection<Payment>(PAYMENTS_COLLECTION)
+            .aggregate([
+                {
+                    $match: {
+                        recipient: req.user!!._id,
+                        status: PaymentStatus.CONFIRMED,
+                    },
+                },
+                {$sort: {timestamp: -1}},
+                {$skip: (page - 1) * pageAmount},
+                {$limit: pageAmount},
+                {
+                    $lookup: {
+                        from: RESOURCES_COLLECTION,
+                        localField: "resource",
+                        foreignField: "_id",
+                        as: "resource",
+                    },
+                },
+                {
+                    $lookup: {
+                        from: USERS_COLLECTION,
+                        localField: "user",
+                        foreignField: "_id",
+                        as: "user",
+                    },
+                },
+                {$unwind: "$user"},
+                {$unwind: "$resource"},
+                {
+                    $unset: [
+                        "user.email",
+                        "user.role",
+                        "user.password",
+                        "user.purchases",
+                    ],
+                },
+            ])
+            .toArray();
 
-    res.success({ payments });
-  }
+        res.success({payments});
+    }
 );
 
 checkoutInfoRouter.post(
-  "/purchase-chart",
-  [
-    body("resource").optional(),
-    body("start", "end")
-      .isNumeric()
-      .bail()
-      .customSanitizer((v) => new Date(v)),
-    Authorize,
-    isValidBody,
-  ],
-  async (req: Request, res: Response) => {
-    let resource = req.body.resource;
+    "/purchase-chart",
+    [
+        body("resource").optional(),
+        body("start", "end")
+            .isNumeric()
+            .bail()
+            .customSanitizer((v) => new Date(v)),
+        Authorize,
+        isValidBody,
+    ],
+    async (req: Request, res: Response) => {
+        let resource = req.body.resource;
 
-    const filter: any = {
-      recipient: req.user!!._id,
-      timestamp: {
-        $lte: new Date(req.body.end),
-        $gte: new Date(req.body.start),
-      },
-    };
-    if (resource) {
-      const result = shortid.isValid(resource);
-      if (!result) {
-        res.failure("invalid resource id.");
-        return;
-      }
-      filter.resource = resource;
+        const filter: any = {
+            recipient: req.user!!._id,
+            status: "CONFIRMED",
+            timestamp: {
+                $lte: new Date(req.body.end),
+                $gte: new Date(req.body.start),
+            },
+        };
+        if (resource) {
+            const result = shortid.isValid(resource);
+            if (!result) {
+                res.failure("invalid resource id.");
+                return;
+            }
+            filter.resource = resource;
+        }
+
+        const payments = await getDatabase()
+            .collection<Payment>(PAYMENTS_COLLECTION)
+            .find(filter)
+            .sort({timestamp: -1})
+            .toArray();
+
+        res.success({payments});
     }
-
-    const payments = await getDatabase()
-      .collection<Payment>(PAYMENTS_COLLECTION)
-      .find(filter)
-      .sort({ timestamp: -1 })
-      .toArray();
-
-    res.success({ payments });
-  }
 );
 
 checkoutInfoRouter.get(
-  "/seller",
-  [Authorize, isValidBody],
-  async (req: Request, res: Response) => {
-    const seller = await getDatabase()
-      .collection<Seller>(SELLER_COLLECTION)
-      .findOne({ user: req.user!!._id });
-    res.success({ isSeller: seller !== null });
-  }
+    "/seller",
+    [Authorize, isValidBody],
+    async (req: Request, res: Response) => {
+        const seller = await getDatabase()
+            .collection<Seller>(SELLER_COLLECTION)
+            .findOne({user: req.user!!._id});
+        res.success({isSeller: seller !== null});
+    }
 );
 
 export default checkoutInfoRouter;
